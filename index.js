e9be3a709ab8f937ab6f8227981c3632bbbe6008
Lsc0223 <liu20120223@outlook.com>
2025-07-20 22:32:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+export default {
+  async fetch(request, env) {
+    const _url = new URL(request.url);
+    _url.hostname = _url.pathname.startsWith("/gh/")
+      ? "alist.906331.xyz"
+      : "disabled-perrine-liu0223liu-c0620a9b.koyeb.app";
+    const req = new Request(_url, request);
+    return fetch(req);
+  },
+};
